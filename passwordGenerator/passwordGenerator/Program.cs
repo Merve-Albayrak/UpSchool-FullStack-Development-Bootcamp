aef84a0c75a368f4e9135e6c90ec022bddaf2af4
Merve-Albayrak <63204791+Merve-Albayrak@users.noreply.github.com>
2023-02-28 19:06:12 +0300

    Delete passwordGenerator directory

--- passwordGenerator/passwordGenerator/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using passwordGenerator;
-
-Console.WriteLine("Hello, World!");
-
-Generate generate = new Generate();
-generate.GeneratePassword();
